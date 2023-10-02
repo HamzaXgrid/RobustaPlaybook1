42829c368e6370e55b080d6dcca14efc5909af72
HamzaXgrid <hamza.arshad@xgrid.co>
2023-10-02 14:40:33 +0500

    Added textblock

--- my_playbook_repo/my-actions.py
+++ my_playbook_repo/my-actions.py
@@ -7,23 +7,26 @@ def list_files_on_persistent_volume(event: PodEvent):
     pod = event.get_pod()
 
     # Specify the path to the Persistent Volume
-    persistent_volume_path = "/mnt/data" 
+    persistent_volume_path = "/mnt/data"
 
     try:
         # List all files in the specified path
         files = os.listdir(persistent_volume_path)
     except Exception as e:
         # Handle any exceptions if the directory doesn't exist or can't be accessed
-        error_message = f"Error: {str(e)}"
-        event.add_enrichment(MarkdownBlock(f"*{error_message}*"))
+        event.add_enrichment([
+            TextBlock(f"Error: {str(e)}")
+        ])
         return
 
     # Prepare a message with the list of files
     file_list_message = f"Files in the Persistent Volume ({persistent_volume_path}):\n"
     file_list_message += "\n".join(files)
 
-    # Create a MarkdownBlock with the file list
-    file_list_block = MarkdownBlock(file_list_message)
+    # Create a text block with the file list
+    file_list_block = TextBlock(file_list_message)
 
     # Add the file list as an enrichment
-    event.add_enrichment(file_list_block)
+    event.add_enrichment([
+        MarkdownBlock(file_list_block)
+    ])
