d2c416634a5b7008221a06c98446b4c3e996de7f
HamzaXgrid <hamza.arshad@xgrid.co>
2023-10-02 15:31:34 +0500

    Got previous configurations

--- my_playbook_repo/my-actions.py
+++ my_playbook_repo/my-actions.py
@@ -1,4 +1,4 @@
-from robusta.api import TextBlock, MarkdownBlock  # Import TextBlock and MarkdownBlock
+from robusta.api import *
 import os
 
 @action
@@ -7,26 +7,23 @@ def list_files_on_persistent_volume(event: PodEvent):
     pod = event.get_pod()
 
     # Specify the path to the Persistent Volume
-    persistent_volume_path = "/mnt/data"
+    persistent_volume_path = "/mnt/data" 
 
     try:
         # List all files in the specified path
         files = os.listdir(persistent_volume_path)
     except Exception as e:
         # Handle any exceptions if the directory doesn't exist or can't be accessed
-        event.add_enrichment([
-            TextBlock(f"Error: {str(e)}")
-        ])
+        error_message = f"Error: {str(e)}"
+        event.add_enrichment(MarkdownBlock(f"*{error_message}*"))
         return
 
     # Prepare a message with the list of files
     file_list_message = f"Files in the Persistent Volume ({persistent_volume_path}):\n"
     file_list_message += "\n".join(files)
 
-    # Create a text block with the file list
-    file_list_block = MarkdownBlock(file_list_message)  # Use MarkdownBlock for Slack formatting
+    # Create a MarkdownBlock with the file list
+    file_list_block = MarkdownBlock(file_list_message)
 
     # Add the file list as an enrichment
-    event.add_enrichment([
-        file_list_block
-    ])
+    event.add_enrichment(file_list_block)
