bb73b399bb57b7296ee2a450ebd7e96277aed355
HamzaXgrid <hamza.arshad@xgrid.co>
2023-10-04 15:36:29 +0500

    Function with output name of PV v2.9.1

--- my_playbook_repo/my-actions.py
+++ my_playbook_repo/my-actions.py
@@ -202,9 +202,10 @@ def volume_analysis4(event: PersistentVolumeEvent):
         persistent_VolumeClaimNameSpace=persistent_Volume.spec.claimRef.namespace
     pv=persistent_Volume.metadata.name
     list_of_Pods=PodList.listNamespacedPod(persistent_VolumeClaimNameSpace).obj
+    print("Pods ARE "+list_of_Pods)
     event.add_enrichment([
         MarkdownBlock("The Name of The PV is " + persistent_VolumeName +persistent_VolumeClaimName + persistent_VolumeClaimNameSpace),
-        FileBlock("PV.log", list_of_Pods)
+        FileBlock("PV.log", pv)
     ])
 
 def persistent_volume_reader(persistent_volume):
