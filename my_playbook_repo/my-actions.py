f44542fc87320f2f99c888b25ca8533d46d19f62
HamzaXgrid <hamza.arshad@xgrid.co>
2023-10-06 11:05:53 +0500

    Function with output name of PV v3.0.2.13

--- my_playbook_repo/my-actions.py
+++ my_playbook_repo/my-actions.py
@@ -27,13 +27,7 @@ import subprocess
 
 
 from robusta.api import *
-import time
 
-from kubernetes import config
-from kubernetes.client import Configuration
-from kubernetes.client.api import core_v1_api
-from kubernetes.client.rest import ApiException
-from kubernetes.stream import stream
 @action
 def my_action(event: PodEvent):
     # we have full access to the pod on which the alert fired
@@ -73,112 +67,28 @@ def volume_analysis6(event: PersistentVolumeEvent):
                 new_podMountPath = podMountPath[1:]
                 print("New path ", new_podMountPath)
                 #break
-        config.load_kube_config()
+        namespace = "default"
+        pod_name = "new-pv-pod"
+        podR = RobustaPod(name=pod_name, namespace=namespace)
+
+        # Define the command to run inside the Pod
+        command = "ls -R"
+
         try:
-            c = Configuration().get_default_copy()
-        except AttributeError:
-            c = Configuration()
-            c.assert_hostname = False
-        Configuration.set_default(c)
-        core_v1 = core_v1_api.CoreV1Api()
+            # Execute the command inside the Pod
+            output = podR.exec_command(command)
 
-        exec_commands(core_v1)
+            # Print the command output
+            print("Command Output:")
+            print(output)
 
+        except Exception as e:
+            print(f"Error executing command in Pod: {str(e)}")
     else:
         event.add_enrichment([
             MarkdownBlock("No PVC is attached to the PV named " + Persistent_Volume_Name)
         ])
 
-def exec_commands(api_instance):
-    name = 'busybox-test'
-    resp = None
-    try:
-        resp = api_instance.read_namespaced_pod(name=name,
-                                                namespace='default')
-    except ApiException as e:
-        if e.status != 404:
-            print(f"Unknown error: {e}")
-            exit(1)
-
-    if not resp:
-        print(f"Pod {name} does not exist. Creating it...")
-        pod_manifest = {
-            'apiVersion': 'v1',
-            'kind': 'Pod',
-            'metadata': {
-                'name': name
-            },
-            'spec': {
-                'containers': [{
-                    'image': 'busybox',
-                    'name': 'sleep',
-                    "args": [
-                        "/bin/sh",
-                        "-c",
-                        "while true;do date;sleep 5; done"
-                    ]
-                }]
-            }
-        }
-        resp = api_instance.create_namespaced_pod(body=pod_manifest,
-                                                  namespace='default')
-        while True:
-            resp = api_instance.read_namespaced_pod(name=name,
-                                                    namespace='default')
-            if resp.status.phase != 'Pending':
-                break
-            time.sleep(1)
-        print("Done.")
-
-    # Calling exec and waiting for response
-    exec_command = [
-        '/bin/sh',
-        '-c',
-        'echo This message goes to stderr; echo This message goes to stdout']
-    # When calling a pod with multiple containers running the target container
-    # has to be specified with a keyword argument container=<name>.
-    resp = stream(api_instance.connect_get_namespaced_pod_exec,
-                  name,
-                  'default',
-                  command=exec_command,
-                  stderr=True, stdin=False,
-                  stdout=True, tty=False)
-    print("Response: " + resp)
-
-    # Calling exec interactively
-    exec_command = ['/bin/sh']
-    resp = stream(api_instance.connect_get_namespaced_pod_exec,
-                  name,
-                  'default',
-                  command=exec_command,
-                  stderr=True, stdin=True,
-                  stdout=True, tty=False,
-                  _preload_content=False)
-    commands = [
-        "echo This message goes to stdout",
-        "echo \"This message goes to stderr\" >&2",
-    ]
-
-    while resp.is_open():
-        resp.update(timeout=1)
-        if resp.peek_stdout():
-            print(f"STDOUT: {resp.read_stdout()}")
-        if resp.peek_stderr():
-            print(f"STDERR: {resp.read_stderr()}")
-        if commands:
-            c = commands.pop(0)
-            print(f"Running command... {c}\n")
-            resp.write_stdin(c + "\n")
-        else:
-            break
-
-    resp.write_stdin("date\n")
-    sdate = resp.readline_stdout(timeout=3)
-    print(f"Server date command returns: {sdate}")
-    resp.write_stdin("whoami\n")
-    user = resp.readline_stdout(timeout=3)
-    print(f"Server user is: {user}")
-    resp.close()
 
 def get_pod_attached_to_pvc(api, pvc_name, pvc_namespace):
     try:
@@ -397,3 +307,93 @@ def list_files_on_persistent_volume(event: PersistentVolumeEvent):
     event.add_enrichment(MarkdownBlock("Files in the Persistent Volume"))
 
 
+def exec_commands(api_instance):
+    name = 'busybox-test'
+    resp = None
+    try:
+        resp = api_instance.read_namespaced_pod(name=name,
+                                                namespace='default')
+    except ApiException as e:
+        if e.status != 404:
+            print(f"Unknown error: {e}")
+            exit(1)
+
+    if not resp:
+        print(f"Pod {name} does not exist. Creating it...")
+        pod_manifest = {
+            'apiVersion': 'v1',
+            'kind': 'Pod',
+            'metadata': {
+                'name': name
+            },
+            'spec': {
+                'containers': [{
+                    'image': 'busybox',
+                    'name': 'sleep',
+                    "args": [
+                        "/bin/sh",
+                        "-c",
+                        "while true;do date;sleep 5; done"
+                    ]
+                }]
+            }
+        }
+        resp = api_instance.create_namespaced_pod(body=pod_manifest,
+                                                  namespace='default')
+        while True:
+            resp = api_instance.read_namespaced_pod(name=name,
+                                                    namespace='default')
+            if resp.status.phase != 'Pending':
+                break
+            time.sleep(1)
+        print("Done.")
+
+    # Calling exec and waiting for response
+    exec_command = [
+        '/bin/sh',
+        '-c',
+        'echo This message goes to stderr; echo This message goes to stdout']
+    # When calling a pod with multiple containers running the target container
+    # has to be specified with a keyword argument container=<name>.
+    resp = stream(api_instance.connect_get_namespaced_pod_exec,
+                  name,
+                  'default',
+                  command=exec_command,
+                  stderr=True, stdin=False,
+                  stdout=True, tty=False)
+    print("Response: " + resp)
+
+    # Calling exec interactively
+    exec_command = ['/bin/sh']
+    resp = stream(api_instance.connect_get_namespaced_pod_exec,
+                  name,
+                  'default',
+                  command=exec_command,
+                  stderr=True, stdin=True,
+                  stdout=True, tty=False,
+                  _preload_content=False)
+    commands = [
+        "echo This message goes to stdout",
+        "echo \"This message goes to stderr\" >&2",
+    ]
+
+    while resp.is_open():
+        resp.update(timeout=1)
+        if resp.peek_stdout():
+            print(f"STDOUT: {resp.read_stdout()}")
+        if resp.peek_stderr():
+            print(f"STDERR: {resp.read_stderr()}")
+        if commands:
+            c = commands.pop(0)
+            print(f"Running command... {c}\n")
+            resp.write_stdin(c + "\n")
+        else:
+            break
+
+    resp.write_stdin("date\n")
+    sdate = resp.readline_stdout(timeout=3)
+    print(f"Server date command returns: {sdate}")
+    resp.write_stdin("whoami\n")
+    user = resp.readline_stdout(timeout=3)
+    print(f"Server user is: {user}")
+    resp.close()
